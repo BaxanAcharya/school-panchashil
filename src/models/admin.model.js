43367e9837c3bf5664c0479324e7cd324bc444f4
BaxanAcharya <baxanacharya@gmail.com>
2024-01-01 23:02:49 +0545

    Admin model added

---
+++ src/models/admin.model.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+const adminSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      index: true,
+    },
+    thumbnail: {
+      type: String,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+adminSchema.pre("save", async (next) => {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+adminSchema.methods.isPasswordCorrect = async (password) => {
+  return await bcrypt.compare(password, this.password);
+};
+adminSchema.methods.generateRefreshToken = async () => {
+  return await jwt.sign({ _id: this._id }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+  });
+};
+adminSchema.methods.generateAccessToken = async () => {
+  return await jwt.sign(
+    { _id: this._id, email: this.email, fullName: this.fullName },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+export const Admin = mongoose.model("Admin", adminSchema);
