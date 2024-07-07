327ed3bd1eae729173a714e563d749310f13a5cd
BaxanAcharya <baxanacharya@gmail.com>
2024-07-07 19:18:01 +0545

    Staff module

---
+++ src/controllers/staff.controller.js
@@ -0,0 +1,111 @@
+import mongoose from "mongoose";
+import Staff from "../models/staff.model.js";
+import { GenericError } from "../utils/GenericError.js";
+import { GenericReponse } from "../utils/GenericResponse.js";
+import { handleAsync } from "../utils/handleAsync.js";
+
+const addStaff = handleAsync(async (req, res) => {
+  const { name, salary, designation, kidsFee } = req.body;
+  if (!name) {
+    return res.status(400).json(new GenericError(400, "Name is required"));
+  }
+  if (typeof salary !== "number" || salary <= 0) {
+    return res.status(400).json(new GenericError(400, "Salary is required"));
+  }
+
+  if (!designation) {
+    return res
+      .status(400)
+      .json(new GenericError(400, "Designation is required"));
+  }
+
+  if (kidsFee && typeof kidsFee !== "number") {
+    return res
+      .status(400)
+      .json(new GenericError(400, "Kids fee should be a number"));
+  }
+
+  const newStaff = await new Staff({
+    name,
+    salary,
+    designation,
+    kidsFee: kidsFee || 0,
+  }).save();
+
+  return res.status(201).json(new GenericReponse(201, "Staff added", newStaff));
+});
+
+const getStaffs = handleAsync(async (req, res) => {
+  const staffs = await Staff.find();
+  return res.status(200).json(new GenericReponse(200, "Staffs", staffs));
+});
+
+const getStaffById = handleAsync(async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json(new GenericError(400, "Invalid id"));
+  }
+  const staff = await Staff.findById(id);
+  if (!staff) {
+    return res.status(404).json(new GenericError(404, "Staff not found"));
+  }
+  return res.status(200).json(new GenericReponse(200, "Staff", staff));
+});
+
+const updateStaff = handleAsync(async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json(new GenericError(400, "Invalid id"));
+  }
+  const staff = await Staff.findById(id);
+  if (!staff) {
+    return res.status(404).json(new GenericError(404, "Staff not found"));
+  }
+  const { name, salary, designation, kidsFee } = req.body;
+  if (!name) {
+    return res.status(400).json(new GenericError(400, "Name is required"));
+  }
+  if (typeof salary !== "number" || salary <= 0) {
+    return res.status(400).json(new GenericError(400, "Salary is required"));
+  }
+  if (!designation) {
+    return res
+      .status(400)
+      .json(new GenericError(400, "Designation is required"));
+  }
+  if (kidsFee && typeof kidsFee !== "number") {
+    return res
+      .status(400)
+      .json(new GenericError(400, "Kids fee should be a number"));
+  }
+  if (name) {
+    staff.name = name;
+  }
+  if (salary !== undefined || salary !== null) {
+    staff.salary = salary;
+  }
+  if (designation) {
+    staff.designation = designation;
+  }
+  if (kidsFee !== undefined || kidsFee !== null) {
+    staff.kidsFee = kidsFee;
+  }
+  await staff.save();
+  return res.status(200).json(new GenericReponse(200, "Staff updated", staff));
+});
+
+const makeStaffLeave = handleAsync(async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json(new GenericError(400, "Invalid id"));
+  }
+  const staff = await Staff.findById(id);
+  if (!staff) {
+    return res.status(404).json(new GenericError(404, "Staff not found"));
+  }
+  staff.hasLeft = !staff.hasLeft;
+  await staff.save();
+  return res.status(200).json(new GenericReponse(200, "Staff left", staff));
+});
+
+export { addStaff, getStaffById, getStaffs, makeStaffLeave, updateStaff };
---
+++ src/models/staff.model.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const staffSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    salary: {
+      type: Number,
+      required: true,
+    },
+    designation: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    kidsFee: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    taxPercentage: {
+      type: Number,
+      required: false,
+      default: 1,
+    },
+    hasLeft: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Staff", staffSchema);
