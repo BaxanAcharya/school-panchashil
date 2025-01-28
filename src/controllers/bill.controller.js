6284ba99e1fa2bf25a73ef33e1afc2c820a426e8
BaxanAcharya <baxanacharya@gmail.com>
2025-01-28 20:34:10 +0545

    Enhance getBills function to calculate detailed fee sums and total amount

--- src/controllers/bill.controller.js
+++ src/controllers/bill.controller.js
@@ -534,9 +534,32 @@ const getBills = handleAsync(async (req, res) => {
     {
       $group: {
         _id: null,
-        totalSum: { $sum: "$total" },
+        admissionFeeSum: { $sum: "$admissionFee.amount" },
+        serviceFeeSum: { $sum: "$serviceFee.amount" },
+        schoolFeeSum: { $sum: "$schoolFee.amount" },
+        stationaryFeeSum: { $sum: "$stationaryFee.amount" },
+        depositSum: { $sum: "$deposit.amount" },
+        transportationSum: { $sum: "$transportation.amount" },
+        evaluationSum: { $sum: "$evaluation.amount" },
+        extraSum: { $sum: "$extra.amount" },
+        diarySum: { $sum: "$diary.amount" },
         paidAmountSum: { $sum: "$paidAmount" },
         discountSum: { $sum: "$discount" },
+        totalSum: {
+          $sum: {
+            $add: [
+              "$admissionFee.amount",
+              "$serviceFee.amount",
+              "$schoolFee.amount",
+              "$stationaryFee.amount",
+              "$deposit.amount",
+              "$transportation.amount",
+              "$evaluation.amount",
+              "$extra.amount",
+              "$diary.amount",
+            ],
+          },
+        },
       },
     },
   ];
@@ -553,6 +576,8 @@ const getBills = handleAsync(async (req, res) => {
       .status(500)
       .json(new GenericError(500, "Error while fetching bills"));
   }
+
+  console.log(totals);
 
   return res.status(200).json(
     new GenericReponse(200, "Bills Fetched Successfully", {
