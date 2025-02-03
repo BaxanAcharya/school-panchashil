d3e0cde08ff1a1c99a81b22216f8bbe1c8db2b5a
BaxanAcharya <baxanacharya@gmail.com>
2025-02-03 12:46:09 +0545

    Replace fs with multer for improved file handling in bucket utility

--- src/utils/bucket.js
+++ src/utils/bucket.js
@@ -1,5 +1,5 @@
 import { v2 as cloudinary } from "cloudinary";
-import fs from "fs";
+import multer from "multer";
 import path from "path";
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
